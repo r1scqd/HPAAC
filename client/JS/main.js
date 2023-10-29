92c14d13dee16696df6fa3a5d481bfca915b159a
Arsen <arssssssssrrrr@gmail.com>
2023-10-29 20:55:11 +0500

    Сделал регистрацию (((

--- client/JS/main.js
+++ client/JS/main.js
@@ -6,6 +6,8 @@ const formSignIn = document.getElementById("form__sign")
 const closeBtn = document.getElementById("close__modal1")
 const closeBtn2 = document.getElementById("close__modal2")
 const btn1 = document.getElementById('submit')
+const btn2 = document.getElementById('submit1')
+
 
 logIn.addEventListener("click", function(event) {
     event.preventDefault();
@@ -70,6 +72,63 @@ btn1.addEventListener('click', (e) => {
             }
 
         })
+
+
+
+
+
+})
+
+btn2.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const name = document.getElementById('ferst__name').value
+    const email = document.getElementById('email2').value
+    const lastname = document.getElementById('last__name').value
+    const middle = document.getElementById('middle__name').value
+
+    const login = document.getElementById('login2').value
+    const password = document.getElementById('pas2').value
+
+    fetch('https://7669-188-186-201-27.ngrok-free.app/api/user', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning":'69420',
+            "Accept": 'application/json',
+
+        },
+        body: JSON.stringify({
+            first_name:name,
+            last_name: lastname,
+            middle_name: middle,
+            organization_id :1,
+            status: 'work',
+            job_title: 'zavod',
+            role:'worker',
+            login: login,
+            password:password
+        })
+    })
+//gegdfsgs
+        .then(response => response.json())
+        .then(data=>{
+            const jsonString = JSON.stringify(data)
+            console.log(data)
+            formSignIn.style.display = "none";
+            formLogIn.style.display = "block";
+            if (data.first_name) {
+
+                localStorage.setItem('user', jsonString)
+                setTimeout(() => {
+                    location.assign('./pc/index.html')
+                }, 1000)
+            } else{
+
+            }
+
+        })
+
 
 
 
